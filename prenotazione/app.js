0efe5dca0fab0a79dd1b930c309c7eeee65143b2
Matteo-x220 <matteosasoris@gmail.com>
2020-02-11 08:45:28 +0100

    Car seats booking page is done

--- prenotazione/app.js
+++ prenotazione/app.js
@@ -13,39 +13,40 @@
 
 */
 
-let peopleSelection = document.getElementById("people") //select da riempire con le option
-let result = document.getElementById("risultato") //select da riempire con le option
-let sendButton = document.getElementById("sendButton") //select da riempire con le option
-let resetButton = document.getElementById("secretResetButton") //select da riempire con le option
-let latestButton = document.getElementById("latestButton") //select da riempire con le option
-let hiddenTextField = document.getElementById("hiddenTextField") //select da riempire con le option
-let displayList = document.getElementById("listaPrenotazioni") //select da riempire con le option
+let peopleSelection = document.getElementById('people') //select da riempire con le option
+let result = document.getElementById('risultato') //select da riempire con le option
+let sendButton = document.getElementById('sendButton') //select da riempire con le option
+let resetButton = document.getElementById('secretResetButton') //select da riempire con le option
+let latestButton = document.getElementById('latestButton') //select da riempire con le option
+let hiddenTextField = document.getElementById('hiddenTextField') //select da riempire con le option
+let displayList = document.getElementById('listaPrenotazioni') //select da riempire con le option
+let spinner = document.getElementById('spinner') //select da riempire con le option
 
 let porcoporcoPeople = [
-  "Chiara",
-  "Lorenzo",
-  "Claudio",
-  "Coradi",
-  "Abeni",
-  "Alessia",
-  "Martina",
-  "Cami",
-  "Nina",
-  "Silvia",
-  "Bianca",
-  "Marta",
-  "Mery",
-  "Consiglio",
-  "Elisa"
+  'Chiara',
+  'Lorenzo',
+  'Claudio',
+  'Coradi',
+  'Abeni',
+  'Alessia',
+  'Martina',
+  'Cami',
+  'Nina',
+  'Silvia',
+  'Bianca',
+  'Marta',
+  'Mery',
+  'Consiglio',
+  'Elisa'
 ].sort()
 
-var dweetName = "PorcoPorcoBooking"
-let nope = "🙅🏻‍♂️"
-let yah = "🙋🏻‍♂️"
+var dweetName = 'PorcoPorcoBooking'
+let nope = '🙅🏻‍♂️'
+let yah = '🙋🏻‍♂️'
 var postiMassimi = 4
 
 porcoporcoPeople.forEach(nome => {
-  let nameEntry = document.createElement("option")
+  let nameEntry = document.createElement('option')
   nameEntry.innerHTML = nome
   nameEntry.value = nome
   peopleSelection.appendChild(nameEntry)
@@ -53,7 +54,7 @@ porcoporcoPeople.forEach(nome => {
 
 const init = () => {
   let selezione = peopleSelection.value
-  if (selezione === "") {
+  if (selezione === '') {
     result.innerHTML = "Pls, scegli il tuo nome dall'elenco"
     return
   }
@@ -72,20 +73,21 @@ function sendDweet(jsonObject) {
 }
 
 function getDweet(dweetName) {
+  spinner.className = 'loader'
   dweetio.get_latest_dweet_for(dweetName, function(err, dweet) {
     var dweet = dweet[0] // Dweet is always an array of 1
     // console.log(dweet.thing) // The generated name
     // console.log(dweet.content) // The content of the dweet
     // console.log(dweet.created) // The create date of the dweet
 
-    let fullContent = ""
+    let fullContent = ''
     Object.values(dweet.content).forEach(letter => {
       fullContent += letter // rimetto apposto il dweet che separa ogni lettera
     })
 
     //console.log("full contet: " + fullContent)
     // var actual_JSON = JSON.parse(JSON.parse(fullContent)) // old
-    var actual_JSON = JSON.parse(JSON.parse(fullContent)) // new
+    var actual_JSON = JSON.parse(fullContent) // new
 
     let personePrenotate = []
     //Object.seal(personePrenotate) // fissa la dimensione
@@ -93,17 +95,30 @@ function getDweet(dweetName) {
 
     let postiObject = Object.values(actual_JSON)[0]
     console.log(postiObject)
+
+    displayList.innerHTML = ''
+    Object.values(postiObject).forEach(seat => {
+      if (seat != '') {
+        let occuato = document.createElement('li')
+        occuato.innerHTML = seat
+        displayList.appendChild(occuato)
+      }
+      // prende le persone già a bordo dall'ultimo dweet
+    })
+
+    spinner.className = 'loaderDisabled'
   })
 }
 
 function addPrenotazione(nomeDaInserire, dweetName) {
+  spinner.className = 'loader'
   dweetio.get_latest_dweet_for(dweetName, function(err, dweet) {
     var dweet = dweet[0] // Dweet is always an array of 1
     // console.log(dweet.thing) // The generated name
     // console.log(dweet.content) // The content of the dweet
     // console.log(dweet.created) // The create date of the dweet
 
-    let fullContent = ""
+    let fullContent = ''
     Object.values(dweet.content).forEach(letter => {
       fullContent += letter // rimetto apposto il dweet che separa ogni lettera
     })
@@ -117,28 +132,28 @@ function addPrenotazione(nomeDaInserire, dweetName) {
     let postiObject = Object.values(actual_JSON)[0]
 
     Object.values(postiObject).forEach(seat => {
-      if (seat != "") {
+      if (seat != '') {
         personePrenotate.push(seat)
       }
       // prende le persone già a bordo dall'ultimo dweet
     })
 
-    let returnMessage = "Stato richiesta: "
+    let returnMessage = 'Stato richiesta: '
     if (!personePrenotate.includes(nomeDaInserire)) {
       if (personePrenotate.length < postiMassimi) {
         personePrenotate.push(nomeDaInserire)
       } else {
-        returnMessage += "Attualmente siamo al completo. "
+        returnMessage += 'Attualmente siamo al completo. '
       }
     } else {
-      returnMessage += "Pare che tu abbia già un posto in macchina. "
+      returnMessage += 'Pare che tu abbia già un posto in macchina. '
     }
-    if (returnMessage === "Stato richiesta: ") {
-      returnMessage += "Prenotazione avvenuta :D " + yah
+    if (returnMessage === 'Stato richiesta: ') {
+      returnMessage += 'Prenotazione avvenuta ' + yah
     }
 
-    console.log("Pre: " + personePrenotate.length)
-    console.log("Pre: " + personePrenotate)
+    console.log('Pre: ' + personePrenotate.length)
+    console.log('Pre: ' + personePrenotate)
 
     // for (
     //   let index = 0;
@@ -151,18 +166,18 @@ function addPrenotazione(nomeDaInserire, dweetName) {
     result.innerHTML = returnMessage
 
     while (personePrenotate.length < 4) {
-      personePrenotate.push("")
+      personePrenotate.push('')
     }
 
-    console.log("Post: " + personePrenotate.length)
-    console.log("Post: " + personePrenotate)
+    console.log('Post: ' + personePrenotate.length)
+    console.log('Post: ' + personePrenotate)
 
     let toBeSent = {
-      "troyota:": {
-        posto1: personePrenotate[0] + "",
-        posto2: personePrenotate[1] + "",
-        posto3: personePrenotate[2] + "",
-        posto4: personePrenotate[3] + ""
+      'troyota:': {
+        posto1: personePrenotate[0] + '',
+        posto2: personePrenotate[1] + '',
+        posto3: personePrenotate[2] + '',
+        posto4: personePrenotate[3] + ''
       }
     }
 
@@ -172,19 +187,20 @@ function addPrenotazione(nomeDaInserire, dweetName) {
     sendDweet(toBeSent)
 
     //console.log(toBeSent["troyota:"])
+    spinner.className = 'loaderDisabled'
   })
 }
 
 const autenticateMe = () => {
   // manculetHash = 746614219
   // manculetHash deriva da "manculetvecio", la passphrase
-  hiddenTextField.style = "visibility: visible"
-  let passphrase = ""
+  hiddenTextField.style = 'visibility: visible'
+  hiddenTextField.value = ''
+  let passphrase = ''
   hiddenTextField.onchange = () => {
     passphrase = hiddenTextField.value
-    //console.log("pollo")
     let manculetHash = stringToHash(passphrase)
-    let secondaChiave = stringToHash("colCaspita")
+    let secondaChiave = stringToHash('colCaspita')
     //console.log(manculetHash ^ secondaChiave)
 
     let cryptoResult = 904009744
@@ -193,14 +209,14 @@ const autenticateMe = () => {
     // secondaChivae = cryptoResult ^ manculetHash
 
     if ((manculetHash ^ secondaChiave) === 904009744) {
-      console.log("okkaido")
-      let defaultJson = {
-        "troyota:": { posto1: "", posto2: "", posto3: "", posto4: "" }
-      }
-      sendDweet(JSON.stringify(defaultJson))
-      // loggato
+      console.log('okkaido')
+      let defaultJson =
+        '{"troyota:": { "posto1": "", "posto2": "", "posto3": "", "posto4": "" }}'
+      let returned = JSON.parse(defaultJson)
+      sendDweet(returned)
+      alert('reset avvenuto')
     } else {
-      hiddenTextField.style = "visibility: hidden"
+      hiddenTextField.style = 'visibility: hidden'
       return
     }
   }
@@ -210,10 +226,10 @@ const autenticateMe = () => {
 
 function loadJSON(callback) {
   var xobj = new XMLHttpRequest()
-  xobj.overrideMimeType("application/json")
-  xobj.open("GET", "./car.json", true) // Replace 'my_data' with the path to your file
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', './car.json', true) // Replace 'my_data' with the path to your file
   xobj.onreadystatechange = function() {
-    if (xobj.readyState == 4 && xobj.status == "200") {
+    if (xobj.readyState == 4 && xobj.status == '200') {
       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
       callback(xobj.responseText)
     }
